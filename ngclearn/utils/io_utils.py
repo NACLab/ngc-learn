638a608488f8fe4eebdbaa0c64a30196abec2127
Alexander Ororbia <ago@hal3.cs.rit.edu>
2024-03-06 18:38:56 -0500

    cleaned up/documented io-utils

--- ngclearn/utils/io_utils.py
+++ ngclearn/utils/io_utils.py
@@ -6,18 +6,40 @@ from jax import numpy as jnp, grad, jit, vmap, random, lax
 import os, sys, pickle
 
 def serialize(fname, object): ## object "saving" routine
+    """
+    Serialization (saving) routine
+
+    Args:
+        fname: file name to serialize to on disk
+
+        object: generic object to serialize
+    """
     fd = open(fname, 'wb')
     pickle.dump(object, fd)
     fd.close()
 
 def deserialize(fname): ## object "loading" routine
+    """
+    Deserialization (loading) routine
+
+    Args:
+        fname: file name from disk to deserialize
+
+    Returns:
+        deserialized object from disk
+    """
     fd = open(fname, 'rb')
     object = pickle.load( fd )
     fd.close()
     return object
 
-
 def makedir(directory):
+    """
+    Creates a folder/directory on disk
+
+    Args:
+        directory: string name of directory/folder to create on disk
+    """
     folders = directory.split('/')
     prefix = ""
     remove = []
@@ -35,5 +57,11 @@ def makedir(directory):
         prefix = prefix + folder + "/"
 
 def makedirs(directories):
+    """
+    Creates a set of folders/directories on disk
+
+    Args:
+        directories: array of string names of directories/folders to create on disk
+    """
     for dir in directories:
         makedir(dir)
