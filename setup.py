d00df3e2733130b66d6075055a7bde1c08855dce
Alexander Ororbia <ago@hal3.cs.rit.edu>
2024-03-07 12:45:01 -0500

    updated setup file

--- setup.py
+++ setup.py
@@ -1,12 +1,13 @@
 from setuptools import setup, find_packages
 
-VERSION = '0.6.0'
+VERSION = '0.9.9'
 DESCRIPTION = 'NGC-Learn'
 LONG_DESCRIPTION = 'Simulation software for building and analyzing arbitrary predictive coding, spiking network, and biomimetic neural systems.'
 
 packages = find_packages()
 # Ensure that we do not pollute the global namespace.
 for p in packages:
+    #print(p)
     assert p == 'ngclearn' or p.startswith('ngclearn.')
 
 # Setting up software package construction
@@ -14,13 +15,13 @@ setup(
        # name of package must match the folder name 'ngclearn'
         name="ngclearn",
         version=VERSION,
-        author="Alexander Ororbia",
+        author="Alexander Ororbia, William Gebhardt",
         author_email="<ago@cs.rit.edu>",
         description=DESCRIPTION,
         long_description=LONG_DESCRIPTION,
         packages=packages,
         license="BSD-3-Clause License",
-        url='https://github.com/ago109/ngc-learn',
+        url='https://github.com/NACLab/ngc-learn',
         install_requires=[], # add any additional packages that need to be installed along with your package
         keywords=['python', 'ngc-learn', 'predictive-processing', 'predictive-coding',
                   'jax', 'spiking-neural-networks', 'biomimetics', 'bionics'],
