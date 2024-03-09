2687594223c464e0b62d9b934eb9ed64339d6795
ago109 <ago109@ist.psu.edu>
2024-03-09 17:22:53 -0500

    cleaned up synapses a bit more

--- ngclearn/components/__init__.py
+++ ngclearn/components/__init__.py
@@ -1,11 +1,15 @@
+## point to rate-coded cell componet types
 from .neurons.rate_coded.rateCell import RateCell
 from .neurons.rate_coded.gaussErrorCell import GaussErrorCell
 from .neurons.rate_coded.laplaceErrorCell import LaplaceErrorCell
+## point to spiking cell component types
 from .neurons.spiking.sLIFCell import SLIFCell
 from .neurons.spiking.LIFCell import LIFCell
 from .other.varTrace import VarTrace
+## point to input encoder component types
 from .input_encoders.bernoulliCell import BernoulliCell
 from .input_encoders.poissonCell import PoissonCell
-from .synapses.hebbian.multifactor.hebbianSynapse import HebbianSynapse
+## point to synapse component types
+from .synapses.hebbian.factor.hebbianSynapse import HebbianSynapse
 from .synapses.hebbian.stdp.traceSTDPSynapse import TraceSTDPSynapse
 from .synapses.hebbian.stdp.expSTDPSynapse import ExpSTDPSynapse
