801b8a4a31ad8a3064bcc7764940397442cd313a
Alexander Ororbia <ago@hal3.cs.rit.edu>
2024-03-24 21:29:44 -0400

    minor edit to init

--- ngclearn/components/neurons/__init__.py
+++ ngclearn/components/neurons/__init__.py
@@ -1,7 +1,7 @@
 ## point to rate-coded cell componet types
-from .rate_coded.rateCell import RateCell
-from .rate_coded.gaussianErrorCell import GaussianErrorCell
-from .rate_coded.laplacianErrorCell import LaplacianErrorCell
+from .graded.rateCell import RateCell
+from .graded.gaussianErrorCell import GaussianErrorCell
+from .graded.laplacianErrorCell import LaplacianErrorCell
 ## point to standard spiking cell component types
 from .spiking.sLIFCell import SLIFCell
 from .spiking.LIFCell import LIFCell
