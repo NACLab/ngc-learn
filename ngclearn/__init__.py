93b324391dd9693146625fbee1720bfcebbfd5b8
Alexander Ororbia <ago@hal3.cs.rit.edu>
2024-03-27 23:15:52 -0400

    sync

--- ngclearn/__init__.py
+++ ngclearn/__init__.py
@@ -5,14 +5,15 @@ from pkg_resources import get_distribution
 
 __version__ = get_distribution('ngclearn').version
 
-required = {'ngclib', 'jax', 'jaxlib'} ## list of core ngclearn dependencies
-installed = {pkg.key for pkg in pkg_resources.working_set}
-missing = required - installed
+if not Path(argv[0]).name == "sphinx-build" or Path(argv[0]).name == "build.py":
+    required = {'ngclib', 'jax', 'jaxlib'} ## list of core ngclearn dependencies
+    installed = {pkg.key for pkg in pkg_resources.working_set}
+    missing = required - installed
 
-for key in required:
-    if key in missing:
-        raise ImportError(str(key) + ", a core dependency of ngclearn, is not " \
-                             "currently installed!")
+    for key in required:
+        if key in missing:
+            raise ImportError(str(key) + ", a core dependency of ngclearn, is not " \
+                                "currently installed!")
 
 ## Needed to preload is called before anything in ngclearn
 import ngclib
