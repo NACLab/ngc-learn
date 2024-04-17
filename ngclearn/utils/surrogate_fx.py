536025d2335a98e910c4811b95b1ed484e8a9a02
Alexander Ororbia <ago@hal3.cs.rit.edu>
2024-04-17 15:56:33 -0400

    tweaked surrogate generation

--- ngclearn/utils/surrogate_fx.py
+++ ngclearn/utils/surrogate_fx.py
@@ -8,11 +8,11 @@ functions below returns two routines:
 1) a spike emission routine `spike(v, v_thr)`, 2) its corresponding surrogate
 derivative routine `spike(j, v, v_thr, params)`.
 """
-from jax import numpy as jnp, random, jit
+from jax import numpy as jnp, random, jit, nn
 from functools import partial
 import time, sys
 
-def straight_through_estimator():
+def straight_through_estimator(get_surr_fx=False):
     """
     The straight-through estimator (STE) applied to binary spike emission
     (the Heaviside function).
@@ -30,9 +30,12 @@ def straight_through_estimator():
     @jit
     def d_spike_fx(v, thr):
         return v * 0 + 1.
-    return spike_fx, d_spike_fx
+    if get_surr_fx == True:
+        return spike_fx, spike_fx, d_spike_fx
+    else:
+        return spike_fx, d_spike_fx
 
-def triangular_estimator():
+def triangular_estimator(get_surr_fx=False):
     """
     The triangular surrogate gradient estimator for binary spike emission.
 
@@ -47,9 +50,13 @@ def triangular_estimator():
         mask = (v < thr).astype(jnp.float32)
         dfx = mask * thr - (1. - mask) * thr
         return dfx
-    return spike_fx, d_spike_fx
+        return dfx
+    if get_surr_fx == True:
+        return spike_fx, spike_fx, d_spike_fx
+    else:
+        return spike_fx, d_spike_fx
 
-def arctan_estimator():
+def arctan_estimator(get_surr_fx=False):
     """
     The arctan surrogate gradient estimator for binary spike emission.
 
@@ -58,19 +65,28 @@ def arctan_estimator():
     | where x = v (membrane potential/voltage)
 
     Returns:
-        spike_fx(x, thr), d_spike_fx(x, thr)
+        ( spike_fx(x, thr), d_spike_fx(x, thr) ) OR
+        ( spike_fx(x, thr), surr_fx(x, thr, args), d_spike_fx(x, thr, args) )
     """
     @jit
     def spike_fx(v, thr):
         return (v > thr).astype(jnp.float32)
-    @jit
-    def d_spike_fx(v, thr=0.):
+    @ji
+    def surr_fx(v, alpha=2.):
         pi = jnp.pi
-        dfx = (1./(1. + jnp.square(v * pi))) * (1./pi)
+        return arctan(v * (alpha/2.) * pi) * (1. / pi)
+    @jit
+    def d_spike_fx(v, thr=0., alpha=2.):
+        pi = jnp.pi
+        divTerm = jnp.square(v * pi * (alpha/2.))
+        dfx = (1./(1. + divTerm)) * (1./pi)
         return dfx
-    return spike_fx, d_spike_fx
+    if get_surr_fx == True:
+        return spike_fx, surr_fx, d_spike_fx
+    else:
+        return spike_fx, d_spike_fx
 
-def secant_lif_estimator():
+def secant_lif_estimator(get_surr_fx=False):
     """
     Surrogate function for computing derivative of (binary) spike function
     with respect to the input electrical current/drive to a leaky
@@ -86,13 +102,23 @@ def secant_lif_estimator():
     | dynamic spiking neurons and fixed feedback weights." Neural computation 29.3
     | (2017): 578-602.
 
+    Args:
+        get_surr_fx: if True, makes this function also return the surrogate
+            function that the derivative corresponds to
+
     Returns:
-        spike_fx(x, thr), d_spike_fx(x, thr, args)
+        ( spike_fx(x, thr), d_spike_fx(x, thr) ) OR
+        ( spike_fx(x, thr), surr_fx(x, thr, args), d_spike_fx(x, thr, args) )
     """
     @jit
     def spike_fx(v, thr):
         #return jnp.where(new_voltage > v_thr, 1, 0)
         return (v > thr).astype(jnp.float32)
+
+    @partial(jit, static_argnums=[4])
+    def surr_fx(j, thr=0., c1=0.82, c2=0.08, omit_scale=False):
+        return jnp.maximum(0, nn.tanh(j * c2) * c1)
+
     @partial(jit, static_argnums=[4])
     def d_spike_fx(j, thr=0., c1=0.82, c2=0.08, omit_scale=True): #c1=0.82, c2=0.08):
         """
@@ -125,4 +151,7 @@ def secant_lif_estimator():
         if omit_scale == False:
             dv_dj = dv_dj * (c1 * c2)
         return dv_dj * mask ## 0 if j < 0, otherwise, use dv/dj for j >= 0
-    return spike_fx, d_spike_fx
+    if get_surr_fx == True:
+        return spike_fx, surr_fx, d_spike_fx
+    else:
+        return spike_fx, d_spike_fx
