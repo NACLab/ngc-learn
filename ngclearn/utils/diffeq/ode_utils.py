19c75418c7179e310507f2e36d854434aa272602
ago109 <ago109@ist.psu.edu>
2024-05-12 19:18:30 -0400

    integrated ralston integrator

--- ngclearn/utils/diffeq/ode_utils.py
+++ ngclearn/utils/diffeq/ode_utils.py
@@ -4,7 +4,8 @@ Routines and co-routines for ngc-learn's differential equation integration backe
 Currently supported back-end forms of integration in ngc-learn include:
 1) Euler integration (RK-1);
 2) Midpoint method (RK-2);
-3) Heun's method (error-corrector RK-2)
+3) Heun's method (error-corrector RK-2); 
+4) Ralston's method (error-corrector RK-2)
 """
 from jax import numpy as jnp, random, jit #, nn
 from functools import partial
@@ -18,7 +19,7 @@ def get_integrator_code(integrationType):
     Args:
         integrationType: string indicating integrator type
             (supported type: rk1` or `euler`, `rk2` or `midpoint`,
-            `rk2_heun` or `heun`)
+            `rk2_heun` or `heun`, `rk2_ralston` or `ralston`)
 
     Returns:
         integator type integer code
@@ -28,8 +29,10 @@ def get_integrator_code(integrationType):
         intgFlag = 1
     elif integrationType == "rk2_heun" or integrationType == "heun": ## Heun's method
         intgFlag = 2
+    elif integrationType == "rk2_ralston" or integrationType == "ralston": ## Ralston's method
+        intgFlag = 3
     # elif integrationType == "rk4": ## Runge-Kutte 4rd order code
-    #     intgFlag = 3
+    #     intgFlag = 4
     else:
         if integrationType != "euler" or integrationType == "rk1":
             print("ERROR: unrecognized integration method {} provided! Defaulting \
@@ -37,8 +40,8 @@ def get_integrator_code(integrationType):
     return intgFlag
 
 @jit
-def _add(y1, y2): ## fast co-routine for simple addition
-    return (y1 + y2)
+def _sum_combine(y1, y2, weight1=1., weight2=1.): ## fast co-routine for simple addition
+    return (y1 * weight1 + y2 * weight2)
 
 @jit
 def _step_forward(t, x, dx_dt, dt, x_scale): ## internal step co-routine
@@ -76,7 +79,9 @@ def step_euler(t, x, dfx, dt, params, x_scale=1.):
 def step_heun(t, x, dfx, dt, params, x_scale=1.):
     """
     Iteratively integrates one step forward via Heun's method, i.e., a
-    second-order Runge-Kutta (RK-2) error-corrected step.
+    second-order Runge-Kutta (RK-2) error-corrected step. This method utilizes
+    two (differential) function evaluations to estimate the solution at a given
+    point in time.
     (Note: ngc-learn internally recognizes "rk2_heun" or "heun" for this routine)
 
     | Reference:
@@ -105,7 +110,7 @@ def step_heun(t, x, dfx, dt, params, x_scale=1.):
     dx_dt = dfx(t, x, params)
     _t, _x = _step_forward(t, x, dx_dt, dt, x_scale)
     _dx_dt = dfx(_t, _x, params)
-    summed_dx_dt = _add(dx_dt, _dx_dt)
+    summed_dx_dt = _sum_combine(dx_dt, _dx_dt)
     _, _x = _step_forward(t, x, summed_dx_dt, dt * 0.5, x_scale)
     return _t, _x
 
@@ -142,4 +147,43 @@ def step_rk2(t, x, dfx, dt, params, x_scale=1.):
     tm, xm = _step_forward(t, x, dx_dt, dt * 0.5, x_scale)
     _dx_dt = dfx(tm, xm, params)
     _t, _x = _step_forward(t, x, _dx_dt, dt, x_scale)
+    return _t, _x
+
+def step_ralston(t, x, dfx, dt, params, x_scale=1.):
+    """
+    Iteratively integrates one step forward via Ralston's method, i.e., a
+    second-order Runge-Kutta (RK-2) error-corrected step. This method utilizes
+    two (differential) function evaluations to estimate the solution at a given
+    point in time.
+    (Note: ngc-learn internally recognizes "rk2_ralston" or "ralston" for this
+    routine)
+
+    | Reference:
+    | Ralston, Anthony. "Runge-Kutta methods with minimum error bounds."
+    | Mathematics of computation 16.80 (1962): 431-437.
+
+    Args:
+        t: current time variable to advance by dt
+
+        x: current variable values to advance/iteratively integrate (at time `t`)
+
+        dfx: (ordinary) differential equation co-routine (as implemented in an
+            ngc-learn component)
+
+        dt: integration time step (also referred to as `h` in mathematics)
+
+        params: tuple containing configuration values/hyper-parameters for the
+            (ordinary) differential equation an ngc-learn component will provide
+
+        x_scale: dampening factor to scale `x` by (Default: 1)
+
+    Returns:
+        variable values iteratively integrated/advanced to next step (`t + dt`)
+    """
+    dx_dt = dfx(t, x, params) ## k1
+    tm, xm = _step_forward(t, x, dx_dt, dt * 0.75, x_scale)
+    _dx_dt = dfx(tm, xm, params)  ## k2
+    ## Note: new step is a weighted combination of k1 and k2
+    summed_dx_dt = _sum_combine(dx_dt, _dx_dt, weight1=(1./3.), weight2=(2./3.))
+    _t, _x = _step_forward(t, x, summed_dx_dt, dt, x_scale)
     return _t, _x
