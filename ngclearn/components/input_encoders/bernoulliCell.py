cf38a9f0db5baa968c0c43424e059895a8bc8d20
Viet Nguyen <vietdungnguyen233@gmail.com>
2024-05-15 17:31:40 -0400

    update refactored bernoulli cell

--- ngclearn/components/input_encoders/bernoulliCell.py
+++ ngclearn/components/input_encoders/bernoulliCell.py
@@ -1,4 +1,9 @@
+# %%
+
 from ngcsimlib.component import Component
+from ngcsimlib.compartment import Compartment
+from ngcsimlib.resolver import resolver
+
 from jax import numpy as jnp, random, jit
 from functools import partial
 import time
@@ -48,76 +53,86 @@ class BernoulliCell(Component):
 
         key: PRNG key to control determinism of any underlying synapses
             associated with this cell
-
-        useVerboseDict: triggers slower, verbose dictionary mode (Default: False)
     """
 
-    ## Class Methods for Compartment Names
-    @classmethod
-    def inputCompartmentName(cls):
-        return 'in'
-
-    @classmethod
-    def outputCompartmentName(cls):
-        return 'out'
-
-    @classmethod
-    def timeOfLastSpikeCompartmentName(cls):
-        return 'tols'
-
-    ## Bind Properties to Compartments for ease of use
-    @property
-    def inputCompartment(self):
-        return self.compartments.get(self.inputCompartmentName(), None)
-
-    @inputCompartment.setter
-    def inputCompartment(self, inp):
-        self.compartments[self.inputCompartmentName()] = inp
-
-    @property
-    def outputCompartment(self):
-        return self.compartments.get(self.outputCompartmentName(), None)
-
-    @outputCompartment.setter
-    def outputCompartment(self, out):
-        self.compartments[self.outputCompartmentName()] = out
-
-    @property
-    def timeOfLastSpike(self):
-        return self.compartments.get(self.timeOfLastSpikeCompartmentName(), None)
-
-    @timeOfLastSpike.setter
-    def timeOfLastSpike(self, t):
-        self.compartments[self.timeOfLastSpikeCompartmentName()] = t
-
     # Define Functions
-    def __init__(self, name, n_units, key=None, useVerboseDict=False, **kwargs):
-        super().__init__(name, useVerboseDict, **kwargs)
-
-        ##Random Number Set up
-        self.key = key
-        if self.key is None:
-            self.key = random.PRNGKey(time.time_ns())
+    def __init__(self, name, n_units, key=None):
+        super().__init__(name)
 
         ##Layer Size Setup
         self.batch_size = 1
         self.n_units = n_units
-        self.reset()
 
-    def verify_connections(self):
-        pass
+        # compartments (state of the cell, parameters, will be changed)
+        self.inputs = Compartment(None) # input compartment
+        self.outputs = Compartment(jnp.zeros((self.batch_size, self.n_units))) # output compartment
+        self.tols = Compartment(jnp.zeros((self.batch_size, self.n_units))) # time of last spike
+        self.key = Compartment(random.PRNGKey(time.time_ns()) if key is None else key)
 
-    def advance_state(self, t, dt, **kwargs):
-        self.key, *subkeys = random.split(self.key, 2)
+    @staticmethod
+    def pure_advance(t, dt, key, inputs, tols):
+        key, *subkeys = random.split(key, 2)
+        outputs = sample_bernoulli(subkeys[0], data=inputs)
+        timeOfLastSpike = update_times(t, outputs, tols)
+        return outputs, timeOfLastSpike, key
 
-        self.outputCompartment = sample_bernoulli(subkeys[0], data=self.inputCompartment)
-        #self.timeOfLastSpike = (1 - self.outputCompartment) * self.timeOfLastSpike + (self.outputCompartment * t)
-        self.timeOfLastSpike = update_times(t, self.outputCompartment, self.timeOfLastSpike)
+    @resolver(pure_advance, output_compartments=['outputs', 'tols', 'key'])
+    def advance(self, vals):
+        outputs, tols, key = vals
+        self.outputs.set(outputs)
+        self.tols.set(tols)
+        self.key.set(key)
 
-    def reset(self, **kwargs):
-        self.inputCompartment = None
-        self.outputCompartment = jnp.zeros((self.batch_size, self.n_units)) #None
-        self.timeOfLastSpike = jnp.zeros((self.batch_size, self.n_units))
+    def reset(self):
+        self.inputs.set(None)
+        self.outputs.set(jnp.zeros((self.batch_size, self.n_units))) #None
+        self.tols.set(jnp.zeros((self.batch_size, self.n_units)))
 
     def save(self, **kwargs):
         pass
+
+
+# Testing
+if __name__ == '__main__':
+    from ngcsimlib.compartment import All_compartments
+    from ngcsimlib.context import Context
+    from ngcsimlib.commands import Command
+    from ngclearn.components.neurons.graded.rateCell import RateCell
+
+    def wrapper(compiled_fn):
+        def _wrapped(*args):
+            # vals = jax.jit(compiled_fn)(*args, compartment_values={key: c.value for key, c in All_compartments.items()})
+            vals = compiled_fn(*args, compartment_values={key: c.value for key, c in All_compartments.items()})
+            for key, value in vals.items():
+                All_compartments[str(key)].set(value)
+            return vals
+        return _wrapped
+
+    class AdvanceCommand(Command):
+        compile_key = "advance"
+        def __call__(self, t=None, dt=None, *args, **kwargs):
+            for component in self.components:
+                component.gather()
+                component.advance(t=t, dt=dt)
+
+    with Context("Bar") as bar:
+        b = BernoulliCell("b", 2)
+        a = RateCell("a2", 2, 0.01)
+        a.j << b.outputs
+        cmd = AdvanceCommand(components=[b, a], command_name="Advance")
+
+    compiled_cmd, arg_order = cmd.compile(loops=1, param_generator=lambda loop_id: [loop_id + 1, 0.1])
+    wrapped_cmd = wrapper(compiled_cmd)
+
+    for i in range(3):
+        b.inputs.set(jnp.asarray([[0.3, 0.7]]))
+        wrapped_cmd()
+        print(f"---[ Step {i} ]---")
+        print(f"[b] inputs: {b.inputs.value}, outputs: {b.outputs.value}, time of last spike: {b.tols.value}")
+        print(f"[a] j: {a.j.value}, j_td: {a.j_td.value}, z: {a.z.value}, zF: {a.zF.value}")
+    b.reset()
+    a.reset()
+    print(f"---[ After reset ]---")
+    print(f"[b] inputs: {b.inputs.value}, outputs: {b.outputs.value}, time of last spike: {b.tols.value}")
+    print(f"[a] j: {a.j.value}, j_td: {a.j_td.value}, z: {a.z.value}, zF: {a.zF.value}")
+
