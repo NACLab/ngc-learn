bfc98af311bd43a9a95a2d4e2ca5154e8c495b56
Alexander Ororbia <ago@hal3.cs.rit.edu>
2024-05-16 19:08:33 -0400

    refactored poisson cell to be compile compliant

--- ngclearn/components/input_encoders/poissonCell.py
+++ ngclearn/components/input_encoders/poissonCell.py
@@ -1,4 +1,6 @@
 from ngcsimlib.component import Component
+from ngcsimlib.compartment import Compartment
+from ngcsimlib.resolver import resolver
 from jax import numpy as jnp, random, jit
 from functools import partial
 import time
@@ -28,7 +30,7 @@ def sample_poisson(dkey, data, dt, fmax=63.75):
 
     Args:
         dkey: JAX key to drive stochasticity/noise
-        
+
         data: sensory data (vector/matrix)
 
         dt: integration time constant
@@ -60,44 +62,6 @@ class PoissonCell(Component):
         useVerboseDict: triggers slower, verbose dictionary mode (Default: False)
     """
 
-    ## Class Methods for Compartment Names
-    @classmethod
-    def inputCompartmentName(cls):
-        return 'in'
-
-    @classmethod
-    def outputCompartmentName(cls):
-        return 'out'
-
-    @classmethod
-    def timeOfLastSpikeCompartmentName(cls):
-        return 'tols'
-
-    ## Bind Properties to Compartments for ease of use
-    @property
-    def inputCompartment(self):
-        return self.compartments.get(self.inputCompartmentName(), None)
-
-    @inputCompartment.setter
-    def inputCompartment(self, inp):
-        self.compartments[self.inputCompartmentName()] = inp
-
-    @property
-    def outputCompartment(self):
-        return self.compartments.get(self.outputCompartmentName(), None)
-
-    @outputCompartment.setter
-    def outputCompartment(self, out):
-        self.compartments[self.outputCompartmentName()] = out
-
-    @property
-    def timeOfLastSpike(self):
-        return self.compartments.get(self.timeOfLastSpikeCompartmentName(), None)
-
-    @timeOfLastSpike.setter
-    def timeOfLastSpike(self, t):
-        self.compartments[self.timeOfLastSpikeCompartmentName()] = t
-
     # Define Functions
     def __init__(self, name, n_units, max_freq=63.75, key=None,
                  useVerboseDict=False, **kwargs):
@@ -114,21 +78,95 @@ class PoissonCell(Component):
         ##Layer Size Setup
         self.batch_size = 1
         self.n_units = n_units
-        self.reset()
 
-    def verify_connections(self):
-        pass
+        self.inputs = Compartment(None) # input compartment
+        self.outputs = Compartment(jnp.zeros((self.batch_size, self.n_units))) # output compartment
+        self.tols = Compartment(jnp.zeros((self.batch_size, self.n_units))) # time of last spike
+        self.key = Compartment(random.PRNGKey(time.time_ns()) if key is None else key)
 
-    def advance_state(self, t, dt, **kwargs):
-        self.key, *subkeys = random.split(self.key, 2)
-        self.outputCompartment = sample_poisson(subkeys[0], data=self.inputCompartment, dt=dt, fmax=self.max_freq)
-        #self.timeOfLastSpike = (1 - self.outputCompartment) * self.timeOfLastSpike + (self.outputCompartment * t)
-        self.timeOfLastSpike = update_times(t, self.outputCompartment, self.timeOfLastSpike)
+        #self.reset()
 
-    def reset(self, **kwargs):
-        self.inputCompartment = None
-        self.outputCompartment = jnp.zeros((self.batch_size, self.n_units)) #None
-        self.timeOfLastSpike = jnp.zeros((self.batch_size, self.n_units))
+    @staticmethod
+    def pure_advance(t, dt, max_freq, key, inputs, tols):
+        key, *subkeys = random.split(key, 2)
+        outputs = sample_poisson(subkeys[0], data=inputs, dt=dt, fmax=max_freq)
+        tols = update_times(t, outputs, tols)
+        return outputs, tols, key
+
+    @resolver(pure_advance, output_compartments=['outputs', 'tols', 'key'])
+    def advance(self, vals):
+        outputs, tols, key = vals
+        self.outputs.set(outputs)
+        self.tols.set(tols)
+        self.key.set(key)
+
+    @staticmethod
+    def pure_reset(batch_size, n_units):
+        return None, jnp.zeros((batch_size, n_units)), jnp.zeros((batch_size, n_units))
+
+    @resolver(pure_reset, output_compartments=['inputs', 'outputs', 'tols'])
+    def reset(self, inputs, outputs, tols):
+        self.inputs.set(inputs)
+        self.outputs.set(outputs)
+        self.tols.set(tols)
 
     def save(self, **kwargs):
         pass
+
+    # def verify_connections(self):
+    #     pass
+
+if __name__ == '__main__':
+    from ngcsimlib.compartment import All_compartments
+    from ngcsimlib.context import Context
+    from ngcsimlib.commands import Command
+    from ngclearn.components.neurons.graded.rateCell import RateCell
+
+    def wrapper(compiled_fn):
+        def _wrapped(*args):
+            # vals = jax.jit(compiled_fn)(*args, compartment_values={key: c.value for key, c in All_compartments.items()})
+            vals = compiled_fn(*args, compartment_values={key: c.value for key, c in All_compartments.items()})
+            for key, value in vals.items():
+                All_compartments[str(key)].set(value)
+            return vals
+        return _wrapped
+
+    class AdvanceCommand(Command):
+        compile_key = "advance"
+        def __call__(self, t=None, dt=None, *args, **kwargs):
+            for component in self.components:
+                component.gather()
+                component.advance(t=t, dt=dt)
+
+    class ResetCommand(Command):
+        compile_key = "reset"
+        def __call__(self, t=None, dt=None, *args, **kwargs):
+            for component in self.components:
+                component.gather()
+                component.reset(t=t, dt=dt)
+
+    dkey = random.PRNGKey(1234)
+    with Context("Bar") as bar:
+        a = PoissonCell("a", n_units=1, max_freq=63.75, key=dkey)
+        advance_cmd = AdvanceCommand(components=[a], command_name="Advance")
+        reset_cmd = ResetCommand(components=[a], command_name="Reset")
+
+    compiled_advance_cmd, _ = advance_cmd.compile()
+    wrapped_advance_cmd = wrapper(jit(compiled_advance_cmd))
+
+    compiled_reset_cmd, _ = reset_cmd.compile()
+    wrapped_reset_cmd = wrapper(jit(compiled_reset_cmd))
+
+    T = 50
+    dt = 1.
+
+    t = 0. ## global clock
+    for i in range(T):
+        a.inputs.set(jnp.asarray([[0.5]]))
+        wrapped_advance_cmd(t, dt)
+        print(f"---[ Step {t} ]---")
+        print(f"[a] inputs: {a.inputs.value}, outputs: {a.outputs.value}, time-of-last-spike: {a.tols.value}")
+        t += dt
+    wrapped_reset_cmd()
+    print(f"---[ After reset ]---")
+    print(f"[a] inputs: {a.inputs.value}, outputs: {a.outputs.value}, time-of-last-spike: {a.tols.value}")
