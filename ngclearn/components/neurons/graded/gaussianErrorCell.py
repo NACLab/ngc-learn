0259baed7aedc448479773614f7fe00f86d3dd81
Viet Dung Nguyen <alexvn.edu@gmail.com>
2024-05-17 19:52:56 -0400

    push gaussian error cell and testing

--- ngclearn/components/neurons/graded/gaussianErrorCell.py
+++ ngclearn/components/neurons/graded/gaussianErrorCell.py
@@ -1,3 +1,5 @@
+# %%
+
 from ngcsimlib.component import Component
 from ngcsimlib.compartment import Compartment
 from ngcsimlib.resolver import resolver
@@ -67,20 +69,20 @@ class GaussianErrorCell(Component): ## Rate-coded/real-valued error unit/cell
     def __init__(self, name, n_units, tau_m=0., leakRate=0., key=None):
         super().__init__(name)
 
+        ##Layer Size Setup
+        self.n_units = n_units
+        self.batch_size = 1
+
         ##Random Number Set up
         self.key = Compartment(random.PRNGKey(time.time_ns()) if key is None else key)
         self.j = Compartment(None) # ## electrical current/ input compartment/to be wired/set. # NOTE: VN: This is never used
         self.L = Compartment(None) # loss compartment
         self.e = Compartment(None) # rate-coded output/ output compartment/to be wired/set. # NOTE: VN: This is never used
-        self.mu = Compartment(jnp.zeros((self.batch_size, self.n_units))) # mean/mean name
+        self.mu = Compartment(jnp.zeros((self.batch_size, self.n_units))) # mean/mean name. input wire
         self.dmu = Compartment(jnp.zeros((self.batch_size, self.n_units))) # derivative mean
-        self.target = Compartment(jnp.zeros((self.batch_size, self.n_units))) # target
+        self.target = Compartment(jnp.zeros((self.batch_size, self.n_units))) # target. input wire
         self.dtarget = Compartment(jnp.zeros((self.batch_size, self.n_units))) # derivative target
         self.modulator = Compartment(jnp.asarray(0.0)) # to be set/consumed
-
-        ##Layer Size Setup
-        self.n_units = n_units
-        self.batch_size = 1
 
     # def verify_connections(self):
     #     self.metadata.check_incoming_connections(self.meanName(), min_connections=1)
@@ -96,7 +98,6 @@ class GaussianErrorCell(Component): ## Rate-coded/real-valued error unit/cell
         modulator = jnp.asarray(0.0) ## use and consume modulator
         return dmu, dtarget, L, modulator
 
-
     @resolver(pure_advance, output_compartments=['dmu', 'dtarget', 'L', 'modulator'])
     def advance(self, dmu, dtarget, L, modulator):
         self.dmu.set(dmu)
@@ -104,14 +105,73 @@ class GaussianErrorCell(Component): ## Rate-coded/real-valued error unit/cell
         self.L.set(L)
         self.modulator.set(modulator)
 
+    @staticmethod
+    def pure_reset(batch_size, n_units):
+        dmu = jnp.zeros((batch_size, n_units))
+        dtarget = jnp.zeros((batch_size, n_units))
+        target = jnp.zeros((batch_size, n_units)) #None
+        mu = jnp.zeros((batch_size, n_units)) #None
+        modulator = jnp.asarray(0.0)
+        return dmu, dtarget, target, mu, modulator
 
-
-    def reset(self, **kwargs):
-        self.derivMean = jnp.zeros((self.batch_size, self.n_units))
-        self.derivTarget = jnp.zeros((self.batch_size, self.n_units))
-        self.target = jnp.zeros((self.batch_size, self.n_units)) #None
-        self.mean = jnp.zeros((self.batch_size, self.n_units)) #None
-        self.modulator = None
+    @resolver(pure_reset, output_compartments=['dmu', 'dtarget', 'target', 'mu', 'modulator'])
+    def reset(self, dmu, dtarget, target, mu, modulator):
+        self.dmu.set(dmu)
+        self.dtarget.set(dtarget)
+        self.target.set(target)
+        self.mu.set(mu)
+        self.modulator.set(modulator)
 
     def save(self, **kwargs):
         pass
+
+# Testing
+if __name__ == '__main__':
+    from ngcsimlib.compartment import All_compartments
+    from ngcsimlib.context import Context
+    from ngcsimlib.commands import Command
+
+    def wrapper(compiled_fn):
+        def _wrapped(*args):
+            # vals = jax.jit(compiled_fn)(*args, compartment_values={key: c.value for key, c in All_compartments.items()})
+            vals = compiled_fn(*args, compartment_values={key: c.value for key, c in All_compartments.items()})
+            for key, value in vals.items():
+                All_compartments[str(key)].set(value)
+            return vals
+        return _wrapped
+
+    class AdvanceCommand(Command):
+        compile_key = "advance"
+        def __call__(self, t=None, dt=None, *args, **kwargs):
+            for component in self.components:
+                component.gather()
+                component.advance(t=t, dt=dt)
+
+    class ResetCommand(Command):
+        compile_key = "reset"
+        def __call__(self, t=None, dt=None, *args, **kwargs):
+            for component in self.components:
+                component.gather()
+                component.reset()
+
+    with Context("Bar") as bar:
+        e = GaussianErrorCell("e", 2)
+        advance_cmd = AdvanceCommand(components=[e], command_name="Advance")
+        reset_cmd = ResetCommand(components=[e], command_name="Reset")
+
+    compiled_advance_cmd, _ = advance_cmd.compile()
+    # wrapped_advance_cmd = wrapper(jit(compiled_advance_cmd))
+    wrapped_advance_cmd = wrapper(compiled_advance_cmd)
+
+    compiled_reset_cmd, _ = reset_cmd.compile()
+    wrapped_reset_cmd = wrapper(compiled_reset_cmd)
+
+    dt = 20.0
+    for t in range(4):
+        e.mu.set(jnp.asarray([[0.1, 0.9]]))
+        e.target.set(jnp.asarray([[1.0, -1.0]]))
+        wrapped_advance_cmd(t, dt)
+        print(f"Step {t} - [e] mu: {e.mu.value}, target: {e.target.value}, dmu: {e.dmu.value}, dtarget: {e.dtarget.value}, L: {e.L.value}, modulator: {e.modulator.value}")
+    wrapped_reset_cmd()
+    print(f"Step {t} - [e] mu: {e.mu.value}, target: {e.target.value}, dmu: {e.dmu.value}, dtarget: {e.dtarget.value}, L: {e.L.value}, modulator: {e.modulator.value}")
+
