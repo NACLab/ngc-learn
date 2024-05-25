3053dfca184d563fd48450b375d9aed22ad67239
ago109 <ago109@ist.psu.edu>
2024-05-24 23:25:02 -0400

    cleaned up spike plot

--- ngclearn/utils/viz/spike_plot.py
+++ ngclearn/utils/viz/spike_plot.py
@@ -4,10 +4,13 @@ import matplotlib.pyplot as plt
 cmap = plt.cm.jet
 import jax.numpy as jnp
 import numpy as np
-from ngclearn.utils.viz.raster import create_raster_plot
 
 def plot_spiking_neuron(curr, mem, spike, ref, dt, thr_line=False,
                     title=False, min_mem_val=0.0, max_mem_val=1.25, fname=None):
+    import matplotlib #.pyplot as plt
+    matplotlib.use('Agg')
+    import matplotlib.pyplot as plt
+    cmap = plt.cm.jet
     """
     Simple plotting function for visualizing the trajectory of a single neuron
     (where its input electrical current, membrane potential value, output
@@ -37,10 +40,18 @@ def plot_spiking_neuron(curr, mem, spike, ref, dt, thr_line=False,
 
         fname: the filename to save this plot as, i.e., /path/to/name.png (Default: lif_analysis.png)
     """
+    spk_ = spike + 0
+    mask = 0.
+    if ref != None:
+        mask = np.greater_equal(ref,dt).astype(np.float32)
+    spk_ = spk_ * (1.0 - mask)
+    stat = np.where(spk_ > 0.)
+    indx = (stat[0] * 1. - 1.).tolist()
+
     x_lim = curr.shape[0]
     y_curr_lim = float(np.amax(curr)) + 0.2
-    fig, ax = plt.subplots(3, figsize=(8,6), sharex=True,
-                        gridspec_kw = {'height_ratios': [1, 1, 0.4]})
+    fig, ax = plt.subplots(2, figsize=(8,6), sharex=True,
+                        gridspec_kw = {'height_ratios': [1, 1]})
     # plot input current
     ax[0].plot(curr, c="tab:blue")
     ax[0].set_ylim([0, y_curr_lim])
@@ -51,22 +62,14 @@ def plot_spiking_neuron(curr, mem, spike, ref, dt, thr_line=False,
 
     # plot membrane potential
     ax[1].plot(mem, c="tab:red")
+    ax[1].vlines(x=indx, ymin=0.95, ymax=1.05,
+                 colors='black', ls='--', lw=5)
     ax[1].set_ylim([min_mem_val, max_mem_val])
     ax[1].set_ylabel("Membrane Potential ($V_t$)")
     if thr_line:
         ax[1].axhline(y=thr_line, alpha=0.25, linestyle="dashed", c="black", linewidth=2)
     plt.xlabel("Time Step $t$")
 
-    spk_ = spike + 0
-    mask = 0.
-    if ref != None:
-        mask = np.greater_equal(ref,dt).astype(np.float32)
-    spk_ = spk_ * (1.0 - mask)
-
-    # Plot output spike using spikeplot
-    create_raster_plot(spk_, ax[2], s=100, c="black")
-    plt.ylabel("Output Spikes")
-    plt.yticks([])
     if fname is None:
         fname = "lif_plot.png"
     plt.tight_layout()
