b7a96548191eb91596a1d4745adc42d04b89e95d
ago109 <ago109@ist.psu.edu>
2024-05-29 16:31:24 -0400

    mods to syn

--- ngclearn/components/synapses/__init__.py
+++ ngclearn/components/synapses/__init__.py
@@ -1,3 +1,4 @@
 from .hebbian.hebbianSynapse import HebbianSynapse
 from .hebbian.traceSTDPSynapse import TraceSTDPSynapse
 from .hebbian.expSTDPSynapse import ExpSTDPSynapse
+#from .hebbian.eventSTDPSynapse import EventSTDPSynapse
