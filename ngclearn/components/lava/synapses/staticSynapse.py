da2f134390bf0dee62961e68766fc49ebd12176a
ago109 <ago109@ist.psu.edu>
2024-05-29 16:32:37 -0400

    added static syn to lava

---
+++ ngclearn/components/lava/synapses/staticSynapse.py
@@ -0,0 +1,73 @@
+from ngcsimlib.component import Component
+from ngcsimlib.compartment import Compartment
+from ngcsimlib.resolver import resolver
+from ngclearn.utils import tensorstats
+
+from jax import numpy as jnp
+import time
+
+class StaticSynapse(Component): ## Lava-compliant fixed/non-evolvable synapse
+
+    # Define Functions
+    def __init__(self, name, weights, dt, Rscale=1., **kwargs):
+        super().__init__(name, **kwargs)
+
+        ## synaptic plasticity properties and characteristics
+        self.batch_size = 1
+        self.shape = weights.shape
+        self.dt = dt
+        self.Rscale = Rscale
+
+        ## pre-computed empty zero pads
+        preVals = jnp.zeros((self.batch_size, self.shape[0]))
+        postVals = jnp.zeros((self.batch_size, self.shape[1]))
+        ## Compartments
+        self.inputs = Compartment(preVals)
+        self.outputs = Compartment(postVals)
+        self.weights = Compartment(weights)
+
+    @staticmethod
+    def _advance_state(dt, Rscale, inputs, weights):
+        outputs = jnp.matmul(inputs, weights) * Rscale
+        return outputs
+
+    @resolver(_advance_state)
+    def advance_state(self, outputs):
+        self.outputs.set(outputs)
+
+    @staticmethod
+    def _reset(batch_size, shape):
+        preVals = jnp.zeros((batch_size, shape[0]))
+        postVals = jnp.zeros((batch_size, shape[1]))
+        return (
+            preVals, # inputs
+            postVals, # outputs
+        )
+
+    @resolver(_reset)
+    def reset(self, inputs, outputs):
+        self.inputs.set(inputs)
+        self.outputs.set(outputs)
+
+    def save(self, directory, **kwargs):
+        file_name = directory + "/" + self.name + ".npz"
+        jnp.savez(file_name, weights=self.weights.value)
+
+    def load(self, directory, **kwargs):
+        file_name = directory + "/" + self.name + ".npz"
+        data = jnp.load(file_name)
+        self.weights.set(data['weights'])
+
+    def __repr__(self):
+        comps = [varname for varname in dir(self) if Compartment.is_compartment(getattr(self, varname))]
+        maxlen = max(len(c) for c in comps) + 5
+        lines = f"[{self.__class__.__name__}] PATH: {self.name}\n"
+        for c in comps:
+            stats = tensorstats(getattr(self, c).value)
+            if stats is not None:
+                line = [f"{k}: {v}" for k, v in stats.items()]
+                line = ", ".join(line)
+            else:
+                line = "None"
+            lines += f"  {f'({c})'.ljust(maxlen)}{line}\n"
+        return lines
