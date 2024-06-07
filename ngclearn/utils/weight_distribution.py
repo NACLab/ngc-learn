bc57ab870803fb543e90c097db3bc9c257afe096
ago109 <ago109@ist.psu.edu>
2024-06-07 15:15:44 -0400

    revised weight dist utils

--- ngclearn/utils/weight_distribution.py
+++ ngclearn/utils/weight_distribution.py
@@ -7,7 +7,7 @@ from jax import numpy as jnp, jit, vmap, random, lax, nn
 ################################################################################
 ## supported distribution initializer configuration generator routines
 
-def constant(value):
+def constant(value, **kwargs):
     """
     Produce a configuration for a constant weight distribution initializer.
 
@@ -17,9 +17,10 @@ def constant(value):
     Returns:
         a constant weight initializer configuration
     """
-    return "constant", value, 0.
+    dist_dict = {"dist": "constant", "value": value}
+    return {**kwargs, **dist_dict}
 
-def fan_in_gaussian():
+def fan_in_gaussian(**kwargs):
     """
     Produce a configuration for a fan-in scaled (centered) Gaussian
     distribution initializer.
@@ -27,9 +28,10 @@ def fan_in_gaussian():
     Returns:
         a fan-in scaled Gaussian distribution configuration
     """
-    return "fan_in_gaussian", 0., 0.
+    dist_dict = {"dist": "fan_in_gaussian"}
+    return {**kwargs, **dist_dict}
 
-def gaussian(mu=0., sigma=1.):
+def gaussian(mu=0., sigma=1., **kwargs):
     """
     Produce a configuration for a Gaussian distribution initializer.
 
@@ -41,23 +43,27 @@ def gaussian(mu=0., sigma=1.):
     Returns:
         a Gaussian distribution configuration
     """
-    return "gaussian", mu, sigma
+    assert sigma >= 0.
+    dist_dict = {"dist": "gaussian", "mu": mu, "sigma": sigma}
+    return {**kwargs, **dist_dict}
 
-def uniform(minval=0., maxval=1.):
+def uniform(amin=0., amax=1., **kwargs):
     """
     Produce a configuration for a uniform distribution initializer.
 
     Args:
-        minval: minimum value/bound of weight values (default: 0)
+        amin: minimum value/bound of weight values (default: 0)
 
-        maxval: maximum value/bound of weight values  (default: 1)
+        amax: maximum value/bound of weight values  (default: 1)
 
     Returns:
         a uniform distribution configuration
     """
-    return "uniform", minval, maxval
+    assert amin < amax
+    dist_dict = {"dist": "uniform", "amin": amin, "amax": amax}
+    return {**kwargs, **dist_dict}
 
-def hollow(scale):
+def hollow(scale, **kwargs):
     """
     Produce a configuration for a constant hollow distribution initializer.
 
@@ -67,37 +73,10 @@ def hollow(scale):
     Returns:
         a constant hollow distribution configuration
     """
-    return "hollow", scale, 0.
+    dist_dict = {"dist": "hollow", "scale": scale}
+    return {**kwargs, **dist_dict}
 
-def hollow_uniform(minval=0., maxval=1.):
-    """
-    Produce a configuration for a hollow-masked uniform distribution initializer.
-
-    Args:
-        minval: minimum value/bound of off-diagonal weight values (default: 0)
-
-        maxval: maximum value/bound of off-diagonal weight values (default: 1)
-
-    Returns:
-        a hollow-masked uniform distribution configuration
-    """
-    return "hollow_uniform", minval, maxval
-
-def hollow_gaussian(mu=0., sigma=1.):
-    """
-    Produce a configuration for a hollow-masked Gaussian distribution initializer.
-
-    Args:
-        mu: mean of off-diagonal weight values (default: 0)
-
-        sigma: standard deviation of off-diagonal weight values (default: 1)
-
-    Returns:
-        a hollow-masked gaussian distribution configuration
-    """
-    return "hollow_gaussian", mu, sigma
-
-def eye(scale):
+def eye(scale, **kwargs):
     """
     Produce a configuration for a constant diagonal/eye distribution initializer.
 
@@ -107,35 +86,8 @@ def eye(scale):
     Returns:
         a constant diagonal/eye distribution configuration
     """
-    return "eye", scale, 0.
-
-def eye_uniform(minval=0., maxval=1.):
-    """
-    Produce a configuration for a diagonal/eye-masked uniform distribution initializer.
-
-    Args:
-        minval: minimum value/bound of (on-)diagonal weight values (default: 0)
-
-        maxval: maximum value/bound of (on-)diagonal weight values (default: 1)
-
-    Returns:
-        a digonal/eye-masked uniform distribution configuration
-    """
-    return "eye_uniform", minval, maxval
-
-def eye_gaussian(mu=0., sigma=1.):
-    """
-    Produce a configuration for a diagonal/eye-masked Gaussian distribution initializer.
-
-    Args:
-        mu: mean of (on-)diagonal weight values (default: 0)
-
-        sigma: standard deviation of (on-)diagonal weight values (default: 1)
-
-    Returns:
-        a diagonal/eye-masked gaussian distribution configuration
-    """
-    return "eye_gaussian", mu, sigma
+    dist_dict = {"dist": "eye", "scale": scale}
+    return {**kwargs, **dist_dict}
 
 ################################################################################
 ## initializer co-routine(s)
@@ -147,68 +99,66 @@ def initialize_params(dkey, init_kernel, shape):
     Args:
         dkey: PRNG key to control determinism of this routine
 
-        init_kernel: triplet/3-tuple with 1st element as a string calling the name
-            of initialization scheme to use
+        init_kernel: dictionary specifying the distribution type and its
+            parameters
 
-            :Note: Currently supported kernel schemes include:
-                ("constant", magnitude, ~ignored~);
-                ("uniform", min_val, max_val);
-                ("fan_in_gaussian", ~ignored, ~ignored~);
-                ("gaussian", mu, sigma) OR ("normal", mu, sigma);
-                ("hollow", off_diagonal_scale, ~ignored~);
-                ("hollow_uniform", off_diagonal_min_val, off_diagonal_max_val);
-                ("hollow_gaussian", off_diagonal_mu, off_diagonal_sigma);
-                ("eye", diagonal_scale, ~ignored~);
-                ("eye_uniform", diagonal_min_val, diagonal_max_val);
-                ("eye_gaussian", diagonal_mu, diagonal_sigma)
+            :Note: Currently supported distribution (dist) kernel schemes include:
+                "constant" (value);
+                "uniform" (amin, amax);
+                "gaussian" (mu, sigma);
+                "fan_in_gaussian" (NO params);
+                "hollow" (scale);
+                "eye" (scale);
+                while currently supported post-processing keyword arguments include:
+                "amin" (clip weights values to be >= amin);
+                "amax" (clip weights values to be <= amin);
+                "hollow" (zero out values along main diagonal);
+                "eye" (zero out off-diagonal values)
 
         shape: tuple containing the dimensions/shape of the tensor to initialize
 
     Returns:
         output (tensor) value
     """
-    initType, *args = init_kernel # get out arguments of initialization kernel
+    dist_type = init_kernel.get("dist")
     params = None
-    if "hollow" in initType: ## hollow-matrix init types
-        if initType == "hollow_uniform": ## hollow-uniform
-            lb, ub = args
-            eps = random.uniform(dkey, shape, minval=lb, maxval=ub)
-            params = (1. - jnp.eye(N=shape[0], M=shape[1])) * eps
-        elif initType == "hollow_gaussian":  ## hollow-normal
-            mu, sigma = args
-            eps = random.uniform(dkey, shape, minval=mu, maxval=sigma)
-            params = (1. - jnp.eye(N=shape[0], M=shape[1])) * eps
-        else: ## constant hollow
-            diagScale, _ = args
-            params = (1. - jnp.eye(N=shape[0], M=shape[1])) * diagScale
-    elif "eye" in initType:  ## diagonal eye matrix init types
-        if initType == "eye_uniform": ## diagonal-uniform
-            lb, ub = args
-            eps = random.uniform(dkey, shape, minval=lb, maxval=ub)
-            params = jnp.eye(N=shape[0], M=shape[1]) * eps
-        elif initType == "eye_gaussian": ## diagonal-normal
-            mu, sigma = args
-            eps = random.uniform(dkey, shape, minval=mu, maxval=sigma)
-            params = jnp.eye(N=shape[0], M=shape[1]) * eps
-        else: ## constant diagonal
-            offDiagScale, _ = args
-            params = jnp.eye(N=shape[0], M=shape[1]) * offDiagScale
-    elif initType == "uniform": ## uniformly distributed values
-        lb, ub = args
-        params = random.uniform(dkey, shape, minval=lb, maxval=ub)
-    elif initType == "fan_in_gaussian": ## fan-in scaled normal distributed values
-        Phi = random.normal(dkey, shape)
-        Phi = Phi * jnp.sqrt(1.0 / (shape[0] * 1.))
-        params = Phi.astype(jnp.float32)
-    elif initType == "gaussian" or initType == "normal": ## gaussian distributed values
-        mu, sigma = args
+    if dist_type == "hollow":
+        diag_scale = init_kernel.get("scale", 1.)
+        params = (1. - jnp.eye(N=shape[0], M=shape[1])) * diag_scale
+    elif dist_type == "eye":
+        off_diag_scale = init_kernel.get("scale", 1.)
+        params = jnp.eye(N=shape[0], M=shape[1]) * off_diag_scale
+    elif dist_type == "gaussian" or dist_type == "normal":
+        mu = init_kernel.get("mu", 0.)
+        sigma = init_kernel.get("sigma", 1.)
         params = random.normal(dkey, shape) * sigma + mu
-    elif initType == "constant": ## constant value(s)
-        scale, _ = args
+    elif dist_type == "uniform":
+        amin = init_kernel.get("amin", 0.)
+        amax = init_kernel.get("amax", 1.)
+        params = random.uniform(dkey, shape, minval=amin, maxval=amax)
+    elif dist_type == "fan_in_gaussian":
+        phi = random.normal(dkey, shape)
+        phi = phi * jnp.sqrt(1.0 / (shape[0] * 1.))
+        params = phi.astype(jnp.float32)
+    elif dist_type == "constant":
+        scale = init_kernel.get("scale", 1.)
         params = jnp.ones(shape) * scale
     else:
         raise RuntimeError(
-            "Initialization scheme (" + initType + ") is not recognized/supported!"
-            )
-    return params
+            "Initialization scheme (" + dist_type + ") is not recognized/supported!"
+        )
+    ## check for any additional distribution post-processing kwargs (e.g., clipping)
+    clip_min = init_kernel.get("amin")
+    clip_max = init_kernel.get("amax")
+    is_hollow = init_kernel.get("hollow", False)
+    is_eye = init_kernel.get("eye", False)
+    if clip_min is not None: ## bound all values to be > clip_min
+        params = jnp.maximum(params, clip_min)
+    if clip_max is not None: ## bound all values to be < clip_max
+        params = jnp.minimum(params, clip_max)
+    if is_hollow: ## apply a hollow mask
+        params = (1. - jnp.eye(N=shape[0], M=shape[1])) * params
+    if is_eye: ## apply an eye/diagonal mask
+        params = jnp.eye(N=shape[0], M=shape[1]) * params
+    return params ## return initial distribution conditions
 
