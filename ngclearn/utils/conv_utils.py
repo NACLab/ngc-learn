54ef9d395877070c310d2f04a0802576605c1809
ago109 <ago109@ist.psu.edu>
2024-06-15 18:45:51 -0400

    update to conv syn

--- ngclearn/utils/conv_utils.py
+++ ngclearn/utils/conv_utils.py
@@ -65,37 +65,37 @@ def _conv_same_transpose_padding(inputs, output, kernel, stride):
         pad_a = kernel - 1
     else:
         pad_a = int(np.ceil(pad_len / 2))
-    print("pad_len = ", pad_len)
+    #print("pad_len = ", pad_len)
     pad_b = pad_len - pad_a
     return ((pad_a, pad_b), (pad_a, pad_b))
 
 
 def _conv_valid_transpose_padding(inputs, output, kernel, stride):
-    print("k : ", kernel, "  s : ", stride, " input : ", inputs)
+    #print("k : ", kernel, "  s : ", stride, " input : ", inputs)
     pad_len = output - ((stride - 1) * (inputs - 1) + inputs - (kernel - 1))
     pad_a = kernel - 1
-    print("pad_len = ", pad_len)
+    #print("pad_len = ", pad_len)
     pad_b = pad_len - pad_a
     return ((pad_a, pad_b), (pad_a, pad_b))
 
 
 def _deconv_valid_transpose_padding(inputs, output, kernel, stride):
-    print(inputs, output, kernel, stride)
+    #print(inputs, output, kernel, stride)
     pad_len = output - ((stride - 1) * (inputs - 1) + inputs - (kernel - 1))
     pad_a = output - 1
-    print("pad_len = ", pad_len)
+    #print("pad_len = ", pad_len)
     pad_b = pad_len - pad_a
     return ((pad_a, pad_b), (pad_a, pad_b))
 
 
 def _deconv_same_transpose_padding(inputs, output, kernel, stride):
-    print(inputs, output, kernel, stride)
+    #print(inputs, output, kernel, stride)
     pad_len = output - ((stride - 1) * (inputs - 1) + inputs - (kernel - 1))
     if stride >= output - 1:
         pad_a = output - 1
     else:
         pad_a = int(np.ceil(pad_len / 2))
-    print("pad_len = ", pad_len)
+    #print("pad_len = ", pad_len)
     pad_b = pad_len - pad_a
     return ((pad_a, pad_b), (pad_a, pad_b))
 
@@ -103,9 +103,9 @@ def _deconv_same_transpose_padding(inputs, output, kernel, stride):
 def deconv2d(inputs, filters, stride_size=1, rhs_dilation=(1, 1),
              padding=((0, 0), (0, 0))):  ## Deconv2D
     dim_numbers = ('NHWC', 'HWIO', 'NHWC')
-    print("---- in deconv ----")
-    print("padding : ", padding)
-    print(inputs.shape, filters.shape)
+    #print("---- in deconv ----")
+    #print("padding : ", padding)
+    #print(inputs.shape, filters.shape)
     # padding = "SAME"
     out = lax.conv_transpose(inputs,  # lhs = image tensor
                              filters,  # rhs = conv kernel tensor
@@ -113,9 +113,9 @@ def deconv2d(inputs, filters, stride_size=1, rhs_dilation=(1, 1),
                              padding,  # padding mode
                              rhs_dilation,  # rhs/kernel dilation
                              dim_numbers)
-    print(out.shape)
+    #print(out.shape)
     # print(out[0,:,:,0])
-    print("----------------------")
+    #print("----------------------")
     return out
 
 
@@ -125,9 +125,9 @@ def conv2d(inputs, filters, stride_size=1, rhs_dilation=(1, 1),
     # padding = ((0,0),(0,0)) #"VALID"
     # padding = "SAME"
     dim_numbers = ('NHWC', 'HWIO', 'NHWC')
-    print("---- in conv ----")
-    print("padding : ", padding)
-    print(inputs.shape, filters.shape)
+    #print("---- in conv ----")
+    #print("padding : ", padding)
+    #print(inputs.shape, filters.shape)
     out = lax.conv_general_dilated(inputs,  # lhs = image tensor
                                    filters,  # rhs = conv kernel tensor
                                    (stride_size, stride_size),  # window strides
@@ -135,6 +135,6 @@ def conv2d(inputs, filters, stride_size=1, rhs_dilation=(1, 1),
                                    lhs_dilation,  # lhs/image dilation
                                    rhs_dilation,  # rhs/kernel dilation
                                    dim_numbers)
-    print(out.shape)
-    print("----------------------")
+    #print(out.shape)
+    #print("----------------------")
     return out
