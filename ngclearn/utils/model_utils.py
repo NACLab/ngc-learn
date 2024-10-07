3259e8cdf619a8a438a107d04d9874fd20ac0364
Faezeh Habibi <155960330+Faezehabibi@users.noreply.github.com>
2024-10-07 15:06:51 -0400

    Update model_utils.py

--- ngclearn/utils/model_utils.py
+++ ngclearn/utils/model_utils.py
@@ -90,10 +90,6 @@ def create_function(fun_name, args=None):
     elif fun_name == "sigmoid":
         fx = sigmoid
         dfx = d_sigmoid
-    elif fun_name == "sine":
-        fx = sine
-        dfx = d_sine
-        omega_0 = args
     elif fun_name == "relu":
         fx = relu
         dfx = d_relu
