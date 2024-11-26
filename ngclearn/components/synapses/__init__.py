a53ad7d8ca8bb7b1a7d809a87ae3fe619a0a63bc
Faezeh Habibi <155960330+Faezehabibi@users.noreply.github.com>
2024-11-26 16:02:05 -0500

    Add patched in __init__.py
    Add patched synapses importing

--- ngclearn/components/synapses/__init__.py
+++ ngclearn/components/synapses/__init__.py
@@ -1,7 +1,11 @@
 from .denseSynapse import DenseSynapse
 from .staticSynapse import StaticSynapse
+
+
 ## short-term plasticity components
 from .STPDenseSynapse import STPDenseSynapse
+
+
 ## dense synaptic components
 from .hebbian.hebbianSynapse import HebbianSynapse
 from .hebbian.STDPSynapse import STDPSynapse
@@ -9,6 +13,8 @@ from .hebbian.traceSTDPSynapse import TraceSTDPSynapse
 from .hebbian.expSTDPSynapse import ExpSTDPSynapse
 from .hebbian.eventSTDPSynapse import EventSTDPSynapse
 from .hebbian.BCMSynapse import BCMSynapse
+
+
 ## conv/deconv synaptic components
 from .convolution.convSynapse import ConvSynapse
 from .convolution.staticConvSynapse import StaticConvSynapse
@@ -18,9 +24,13 @@ from .convolution.deconvSynapse import DeconvSynapse
 from .convolution.staticDeconvSynapse import StaticDeconvSynapse
 from .convolution.hebbianDeconvSynapse import HebbianDeconvSynapse
 from .convolution.traceSTDPDeconvSynapse import TraceSTDPDeconvSynapse
+
+
 ## modulated synaptic components
 from .modulated.MSTDPETSynapse import MSTDPETSynapse
+
 ## patched synaptic components
-## from .patched.patchedSynapse import PatchedSynapse
-## from .patched.staticPatchedSynapse import StaticPatchedSynapse
-## from .patched.hebbianPatchedSynapse import HebbianPatchedSynapse
+from .patched.patchedSynapse import PatchedSynapse
+from .patched.staticPatchedSynapse import StaticPatchedSynapse
+from .patched.hebbianPatchedSynapse import HebbianPatchedSynapse
+
