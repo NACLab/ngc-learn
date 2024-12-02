23473abfcbab102bda0f2c9d5520f7cc353541dc
Faezeh Habibi <155960330+Faezehabibi@users.noreply.github.com>
2024-12-02 15:09:24 -0500

    Add patched synapse (#80)
    * Update __init__.py
    
    Add point to patched components
    
    * Add patched in __init__.py
    
    Add patched synapses importing
    
    * Add patched synaptic components
    
    * Delete ngclearn/components/synapses/patched/__pycache__ directory
    
    * Update __init__.py
    
    new line characters added
    
    * Update hebbianPatchedSynapse.py
    
    * Update patchedSynapse.py
    
    new line characters added
    
    * Update staticPatchedSynapse.py
    
    new line characters added
    
    * Update staticPatchedSynapse.py
    
    New line characters + comments for describing each input vars
    
    * Update patchedSynapse.py
    
    Removed a comment line
    
    * Update hebbianPatchedSynapse.py
    
    remove unused arguments
    
    * Update hebbianPatchedSynapse.py
    
    * Update hebbianPatchedSynapse.py
    
    add description for w_mask
    
    * Update hebbianPatchedSynapse.py
    
    * Update hebbianPatchedSynapse.py
    
    * Update patchedSynapse.py
    
    * Update patchedSynapse.py
    
    * Update hebbianPatchedSynapse.py

--- ngclearn/components/__init__.py
+++ ngclearn/components/__init__.py
@@ -1,9 +1,13 @@
 from .jaxComponent import JaxComponent
+
+
 ## point to rate-coded cell component types
 from .neurons.graded.rateCell import RateCell
 from .neurons.graded.gaussianErrorCell import GaussianErrorCell
 from .neurons.graded.laplacianErrorCell import LaplacianErrorCell
 from .neurons.graded.rewardErrorCell import RewardErrorCell
+
+
 ## point to standard spiking cell component types
 from .neurons.spiking.sLIFCell import SLIFCell
 from .neurons.spiking.LIFCell import LIFCell
@@ -12,14 +16,20 @@ from .neurons.spiking.quadLIFCell import QuadLIFCell
 from .neurons.spiking.adExCell import AdExCell
 from .neurons.spiking.fitzhughNagumoCell import FitzhughNagumoCell
 from .neurons.spiking.izhikevichCell import IzhikevichCell
+
+
 ## point to transformer/operater component types
 from .other.varTrace import VarTrace
 from .other.expKernel import ExpKernel
 from ngclearn.components.synapses.modulated.eligibilityTrace import EligibilityTrace
+
+
 ## point to input encoder component types
 from .input_encoders.bernoulliCell import BernoulliCell
 from .input_encoders.poissonCell import PoissonCell
 from .input_encoders.latencyCell import LatencyCell
+
+
 ## point to synapse component types
 from .synapses.denseSynapse import DenseSynapse
 from .synapses.staticSynapse import StaticSynapse
@@ -30,6 +40,8 @@ from .synapses.hebbian.expSTDPSynapse import ExpSTDPSynapse
 from .synapses.hebbian.eventSTDPSynapse import EventSTDPSynapse
 from .synapses.hebbian.BCMSynapse import BCMSynapse
 from .synapses.STPDenseSynapse import STPDenseSynapse
+
+
 ## point to convolutional component types
 from .synapses.convolution.convSynapse import ConvSynapse
 from .synapses.convolution.staticConvSynapse import StaticConvSynapse
@@ -41,5 +53,13 @@ from .synapses.convolution.hebbianDeconvSynapse import HebbianDeconvSynapse
 from .synapses.convolution.traceSTDPDeconvSynapse import TraceSTDPDeconvSynapse
 ## point to modulated component types
 from .synapses.modulated.MSTDPETSynapse import MSTDPETSynapse
+
+
 ## point to monitors
 from .monitor import Monitor
+
+## point to patched component types 
+from .synapses.patched.patchedSynapse import PatchedSynapse
+from .synapses.patched.staticPatchedSynapse import StaticPatchedSynapse
+from .synapses.patched.hebbianPatc
+
---
+++ ngclearn/components/synapses/patched/__init__.py
@@ -0,0 +1,7 @@
+## patched synaptic components
+from .patchedSynapse import PatchedSynapse
+from .staticPatchedSynapse import StaticPatchedSynapse
+from .hebbianPatchedSynapse import HebbianPatchedSynapse
+
+
+
