537c29d933c35555e8f0ae8e8309e29c812fbed8
Faezeh Habibi <155960330+Faezehabibi@users.noreply.github.com>
2024-12-09 21:07:38 -0500

    Update __init__.py
    add dot

--- ngclearn/modules/__init__.py
+++ ngclearn/modules/__init__.py
@@ -1,6 +1,6 @@
-from regression.elastic_net import Iterative_ElasticNet
-from regression.lasso import Iterative_Lasso
-from regression.ridge import Iterative_Ridge
+from .regression.elastic_net import Iterative_ElasticNet
+from .regression.lasso import Iterative_Lasso
+from .regression.ridge import Iterative_Ridge
 
 
 
