da2f24eddfb405151ff512f51ed040ec89529f6d
Faezeh Habibi <155960330+Faezehabibi@users.noreply.github.com>
2024-12-16 16:42:44 -0500

    Add attribute 'lr' (#90)
    * Update elastic_net.py
    
    * add lr as attribute to lasso.py
    
    * add lr as attribute to ridge.py
    
    * refactor w_bound=0. for weights elastic_net.py
    
    deactivated w_bound for weights elastic_net.py
    
    * Update lasso.py
    
    * deactivated w_bound for weights ridge.py

--- ngclearn/modules/regression/elastic_net.py
+++ ngclearn/modules/regression/elastic_net.py
@@ -73,12 +73,14 @@ class Iterative_ElasticNet():
         self.weight_fill = weight_fill
         self.threshold = threshold
         self.name = name
+        self.lr = lr
         feature_dim = dict_dim
 
         with Context(self.name) as self.circuit:
-            self.W = HebbianSynapse("W", shape=(feature_dim, sys_dim), eta=lr,
+            self.W = HebbianSynapse("W", shape=(feature_dim, sys_dim), eta=self.lr,
                                    sign_value=-1, weight_init=dist.constant(weight_fill),
-                                   prior=('elastic_net', (lmbda, l1_ratio)), optim_type=optim_type, key=subkeys[0])
+                                   prior=('elastic_net', (lmbda, l1_ratio)), w_bound=0.,
+                                   optim_type=optim_type, key=subkeys[0])
             self.err = GaussianErrorCell("err", n_units=sys_dim)
 
             # # ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
