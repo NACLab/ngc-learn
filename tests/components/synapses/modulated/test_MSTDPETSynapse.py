94477b8f3391a110c75e089524bd227b9418ac66
Alexander Ororbia <ago@hal3.cs.rit.edu>
2025-11-07 13:02:11 -0500

    refactored mstdpet-syn and test passed

--- tests/components/synapses/modulated/test_MSTDPETSynapse.py
+++ tests/components/synapses/modulated/test_MSTDPETSynapse.py
@@ -2,15 +2,11 @@ from jax import numpy as jnp, random, jit
 from ngcsimlib.context import Context
 import numpy as np
 np.random.seed(42)
-from ngclearn.components import MSTDPETSynapse
-from ngcsimlib.compilers import compile_command, wrap_command
-from numpy.testing import assert_array_equal
 
-from ngcsimlib.compilers.process import Process, transition
-from ngcsimlib.component import Component
-from ngcsimlib.compartment import Compartment
-from ngcsimlib.context import Context
+from ngclearn import Context, MethodProcess
 import ngclearn.utils.weight_distribution as dist
+from ngclearn.components.synapses.modulated.MSTDPETSynapse import MSTDPETSynapse
+from numpy.testing import assert_array_equal
 
 def test_MSTDPETSynapse1():
     name = "mstdpet_ctx"
@@ -24,30 +20,14 @@ def test_MSTDPETSynapse1():
             name="a", shape=(1,1), A_plus=1., A_minus=1., eta=0.1, key=subkeys[0]
         )
 
-        #"""
-        advance_process = (Process("advance_proc")
+        evolve_process = (MethodProcess("evolve_process")
+                          >> a.evolve)
+
+        advance_process = (MethodProcess("advance_proc")
                            >> a.advance_state)
-        # ctx.wrap_and_add_command(advance_process.pure, name="run")
-        ctx.wrap_and_add_command(jit(advance_process.pure), name="run")
 
-        evolve_process = (Process("evolve_proc")
-                           >> a.evolve)
-        #ctx.wrap_and_add_command(evolve_process.pure, name="run")
-        ctx.wrap_and_add_command(jit(evolve_process.pure), name="adapt")
-        
-        reset_process = (Process("reset_proc")
+        reset_process = (MethodProcess("reset_proc")
                          >> a.reset)
-        ctx.wrap_and_add_command(jit(reset_process.pure), name="reset")
-        #"""
-
-        """
-        reset_cmd, reset_args = ctx.compile_by_key(a, compile_key="reset")
-        ctx.add_command(wrap_command(jit(ctx.reset)), name="reset")
-        advance_cmd, advance_args = ctx.compile_by_key(a, compile_key="advance_state")
-        ctx.add_command(wrap_command(jit(ctx.advance_state)), name="run")
-        evolve_cmd, evolve_args = ctx.compile_by_key(a, compile_key="evolve")
-        ctx.add_command(wrap_command(jit(ctx.evolve)), name="adapt")
-        """
 
     a.weights.set(jnp.ones((1, 1)) * 0.75)
 
@@ -59,28 +39,28 @@ def test_MSTDPETSynapse1():
     r_pos = jnp.ones((1, 1))
 
     #print(a.weights.value)
-    ctx.reset()
+    reset_process.run()  # ctx.reset()
     a.preSpike.set(in_spike * 0)
     a.preTrace.set(in_trace)
     a.postSpike.set(out_spike)
     a.postTrace.set(out_trace)
     a.modulator.set(r_pos)
-    ctx.run(t=1. * dt, dt=dt)
-    ctx.adapt(t=1. * dt, dt=dt)
-    ctx.adapt(t=1. * dt, dt=dt)
-    #print(a.weights.value)
-    assert_array_equal(a.weights.value, jnp.array([[0.875]]))
+    advance_process.run(t=1., dt=dt)  # ctx.run(t=1. * dt, dt=dt)
+    evolve_process.run(t=1., dt=dt)  # ctx.adapt(t=1. * dt, dt=dt)
+    evolve_process.run(t=1., dt=dt)  # ctx.adapt(t=1. * dt, dt=dt)
+    #print(a.weights.get())
+    assert_array_equal(a.weights.get(), jnp.array([[0.875]]))
 
-    ctx.reset()
+    reset_process.run()  # ctx.reset()
     a.preSpike.set(in_spike * 0)
     a.preTrace.set(in_trace)
     a.postSpike.set(out_spike)
     a.postTrace.set(out_trace)
     a.modulator.set(r_neg)
-    ctx.run(t=1. * dt, dt=dt)
-    ctx.adapt(t=1. * dt, dt=dt)
-    ctx.adapt(t=1. * dt, dt=dt)
-    #print(a.weights.value)
-    assert_array_equal(a.weights.value, jnp.array([[0.75]]))
+    advance_process.run(t=1., dt=dt)  # ctx.run(t=1. * dt, dt=dt)
+    evolve_process.run(t=1., dt=dt)  # ctx.adapt(t=1. * dt, dt=dt)
+    evolve_process.run(t=1., dt=dt)  # ctx.adapt(t=1. * dt, dt=dt)
+    #print(a.weights.get())
+    assert_array_equal(a.weights.get(), jnp.array([[0.75]]))
 
 #test_MSTDPETSynapse1()
