367a47694ff9f389940af8bba38a651fea0894e4
Faezeh Habibi <155960330+Faezehabibi@users.noreply.github.com>
2026-01-16 21:52:22 -0500

    Add RetinalGanglionCell import to input_encoders

--- ngclearn/components/input_encoders/__init__.py
+++ ngclearn/components/input_encoders/__init__.py
@@ -1,5 +1,7 @@
 from .bernoulliCell import BernoulliCell
 from .poissonCell import PoissonCell
 from .latencyCell import LatencyCell
+from .ganglionCell import RetinalGanglionCell
 from .phasorCell import PhasorCell
+
 
