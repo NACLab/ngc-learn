5eb6180983c7cf8d9451c2cbc41b0cb14339e6e6
Will Gebhardt <will@gebhardts.net>
2026-04-25 20:49:43 -0400

    Update JaxProcessesMixin.py
    Added automatic jit wrapping which is on by default, add "use_jit=False" to a process to disable

--- ngclearn/utils/JaxProcessesMixin.py
+++ ngclearn/utils/JaxProcessesMixin.py
@@ -2,13 +2,37 @@ from ngcsimlib import JointProcess, MethodProcess
 from ngcsimlib.global_state import stateManager
 import jax
 from typing import TYPE_CHECKING
+
+from ngcsimlib._src.parser.utils import CompiledMethod
+
 if TYPE_CHECKING:
     from ngcsimlib._src.process.baseProcess import BaseProcess
 
+class JaxCompiledMethod(CompiledMethod):
+    def __init__(self, fn, fn_ast, auxiliary_ast, namespace, extra_globals):
+        super().__init__(fn, fn_ast, auxiliary_ast, namespace, extra_globals)
+        self._fn = jax.jit(fn)
+        self._fn_source = fn
+
+    @property
+    def source_fn(self):
+        return self._fn_source
+
+    @classmethod
+    def wrap(cls, compiledMethod: CompiledMethod):
+        return cls(compiledMethod._fn,
+                   compiledMethod.ast,
+                   compiledMethod.auxiliary_ast,
+                   compiledMethod.namespace,
+                   compiledMethod.extra_globals)
+
+
 class JaxProcessesMixin:
-    def __init__(self: "BaseProcess"):
+    def __init__(self: "BaseProcess", name, *args, use_jit=True, **kwargs):
+        super().__init__(name, *args, **kwargs)
         self._previous_result = None
         self._previous_state = None
+        self._use_jit = use_jit
 
     @property
     def previous_result(self):
@@ -32,10 +56,14 @@ class JaxProcessesMixin:
             self._previous_result = result
         return final_state, result
 
+    def compile(self: "baseProcess"):
+        super().compile()
+        if self._use_jit:
+            self.run.compiled = JaxCompiledMethod.wrap(self.run.compiled)
 
 
-class JaxJointProcess(JointProcess, JaxProcessesMixin):
+class JaxJointProcess(JaxProcessesMixin, JointProcess):
     pass
 
-class JaxMethodProcess(MethodProcess, JaxProcessesMixin):
+class JaxMethodProcess(JaxProcessesMixin, MethodProcess):
     pass
